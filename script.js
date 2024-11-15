9574a9031724bfc1c0ba3810e8d2927125e51115
Jeremy-PR <jeremy.pelisse@gmail.com>
2024-11-15 12:26:14 +0100

    exo 3eme page

--- script.js
+++ script.js
@@ -1,2 +1,60 @@
-// exercice 1 : 
+// exercice 1 :
 
+// code hamza
+
+// let iterations;
+// let resultat="";
+
+// while(Number.isInteger(iterations) === false ){
+//     iterations = Number(prompt("Veuillez saisir un nombre : "));
+// }
+
+// for (let i = 1; i <= iterations; i++) {
+//   let star = "";
+
+//   for (let x = 0; x < i; x++) {
+//     star += "*";
+//   }
+//  resultat += star + "\n"
+// }
+
+// console.log(resultat);
+
+
+
+
+// exercice 2
+
+// const superhéros = ["spiderman", "Batman", "Robin", "Captain América"];
+// let sup = "";
+
+// superhéros.forEach(sup => {
+//     console.log("je suis fan de " + sup );
+    
+// })
+
+
+
+
+// exercice 3
+
+// let compteRebours = 10;
+
+// while (compteRebours >= 0) {
+//   console.log(compteRebours);
+//   compteRebours--;
+// }
+
+// console.log("Décollage !");
+
+
+
+
+// exercice 4
+
+const amis = ["Ali", "Hamza", "Clément", "Karl"]
+
+for (let i = 0; i < amis.length; i++) {
+   
+    
+}
